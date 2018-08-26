ec1d248229eb590a03b9c51d49733dd3727c0a6c
kevin <kjwilde@hotmail.com>
2018-08-26 01:33:18 -0500

    Setup db user for local mongo.

--- scripts/mongodb/deploy-mongo.sh
+++ scripts/mongodb/deploy-mongo.sh
@@ -8,14 +8,19 @@ export MONGO_PROD_TEST_PASS=$4
 
 mongoUser=$5
 mongoPass=$6
-mongoDb=$7
+
+serviceAdminUser=$7
+serviceAdminPass=$8
+
+mongoDb=$9
 
 
-
+#Add test database and production database
 helm --kube-context $context --namespace $namespace install --name gateway-db --set mongodbRootPassword=admin,mongodbUsername=${MONGO_PROD_TEST_USER},mongodbPassword=${MONGO_PROD_TEST_PASS},mongodbDatabase=${mongoDb} ../../helm-charts/mongodb
 
 #./create-mongo-user.sh ${mongoUser} ${mongoPass} ${mongoDb}
 ./create-mongo-user.sh ${mongoUser} ${mongoPass} admin
+./create-mongo-user.sh ${serviceAdminUser} ${serviceAdminPass} admin
 
 #./deploy-mongo.sh minikube cryptocurrency-services test-user test-pass admin admin cryptocurrency-services-local-test
 
